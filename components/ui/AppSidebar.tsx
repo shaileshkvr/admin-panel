0f2b943bdec42a8a2c6e2b2afa678f6dcc2dcc45
Shailesh <126746445+shaileshkvr@users.noreply.github.com>
2025-11-21 15:38:15 +0530

    feat: Enhance AppSidebar with dynamic menu items and user dropdown

--- components/ui/AppSidebar.tsx
+++ components/ui/AppSidebar.tsx
@@ -1,7 +1,103 @@
-const AppSidebar = () => {
-  return (
-    <div className=''>AppSidebar</div>
-  )
-}
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarSeparator,
+} from "./sidebar";
+import Link from "next/link";
 
-export default AppSidebar
+import {
+  User,
+  Inbox,
+  Calendar,
+  Search,
+  Settings,
+  User2,
+  ChevronUp,
+} from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+} from "./dropdown-menu";
+// import Image from "next/image";
+
+const AppSidebar = () => {
+  const contentArr = [
+    { id: 1, title: "Home", url: "#", icon: User },
+    { id: 2, title: "Inbox", url: "#", icon: Inbox },
+    { id: 3, title: "Calendar", url: "#", icon: Calendar },
+    { id: 4, title: "Search", url: "#", icon: Search },
+    { id: 5, title: "Settings", url: "#", icon: Settings },
+  ];
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href="/">
+              {/* <Image src="" alt="" /> */}
+              <span>Fuzzy Trends</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarSeparator />
+      {/* Sidebar content */}
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {contentArr.map((item) => (
+                <SidebarMenuItem key={item.id}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      {/* Sidebar Footer Content */}
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 />
+                  Shailesh Verma
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem>Account</DropdownMenuItem>
+                <DropdownMenuItem>Settings</DropdownMenuItem>
+                <DropdownMenuItem variant="destructive">
+                  Sign Out
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+};
+
+export default AppSidebar;
