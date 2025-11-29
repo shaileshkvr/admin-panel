dc8fedf71e0bff466c939c25f590afa46a62ecb2
Shailesh <126746445+shaileshkvr@users.noreply.github.com>
2025-11-29 14:32:48 +0530

    feat: Update HomePage to include area, bar, and pie charts

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
-const Page = () => {
-  return <div className="w-full flex justify-center items-center">Home</div>;
+import AppAreaChart from "@/components/ui/AppAreaChart";
+import AppBarChart from "@/components/ui/AppBarChart";
+import AppPieChart from "@/components/ui/AppPieChart";
+import App from "next/app";
+
+const HomePage = () => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4 px-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <AppAreaChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppPieChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">test</div>
+    </div>
+  );
 };
 
-export default Page;
+export default HomePage;
