ad005f7f832df43a0f26151d59fc43b880c3d1fe
Shailesh <126746445+shaileshkvr@users.noreply.github.com>
2025-12-19 20:31:12 +0530

    feat: update sidebar menu items with correct Home URL and icon

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -13,9 +13,11 @@ import {
   SidebarMenuItem,
   SidebarSeparator,
 } from "@/components/ui/sidebar";
+
 import Link from "next/link";
 
 import {
+  Home,
   User,
   Inbox,
   Calendar,
@@ -49,11 +51,12 @@ const AppSidebar = () => {
     url: string;
     icon: LucideIcon;
   }[] = [
-    { id: 1, title: "Home", url: "/users", icon: User },
-    { id: 2, title: "Inbox", url: "#", icon: Inbox },
-    { id: 3, title: "Calendar", url: "#", icon: Calendar },
-    { id: 4, title: "Search", url: "#", icon: Search },
-    { id: 5, title: "Settings", url: "#", icon: Settings },
+    { id: 1, title: "Home", url: "/", icon: Home },
+    { id: 2, title: "Users", url: "/users", icon: User },
+    { id: 3, title: "Inbox", url: "#", icon: Inbox },
+    { id: 4, title: "Calendar", url: "#", icon: Calendar },
+    { id: 5, title: "Search", url: "#", icon: Search },
+    { id: 6, title: "Settings", url: "#", icon: Settings },
   ];
   return (
     <Sidebar collapsible="icon">
