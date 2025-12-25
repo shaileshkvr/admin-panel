62b317c689e65afa8965fa605a44cc04f175054a
Shailesh <126746445+shaileshkvr@users.noreply.github.com>
2025-12-25 20:10:13 +0530

    feat: enhance Edit User component with admin token validation and role-based fields

--- components/EditUser.tsx
+++ components/EditUser.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useForm, Controller } from "react-hook-form";
+import { useForm, Controller, useWatch } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 
@@ -35,26 +35,48 @@ import {
   UsersIcon,
 } from "lucide-react";
 
-const formSchema = z.object({
-  username: z
-    .string()
-    .min(3, "Name must be at least 3 characters")
-    .max(30, "Name must not exceed 30 characters"),
-  email: z.string().email("Invalid email address"),
-  phone: z.string().min(10).max(15).optional(),
-  location: z.string().min(2),
-  role: z.enum(["admin", "user"]),
-});
+const formSchema = z
+  .object({
+    username: z
+      .string()
+      .min(3, "Name must be at least 3 characters")
+      .max(30, "Name must not exceed 30 characters"),
+    email: z.email("Invalid email address"),
+    phone: z
+      .string()
+      .regex(/^\d{10,15}$/, "Invalid phone number")
+      .optional(),
+    location: z.string().min(2),
+    role: z.enum(["admin", "user"]),
+    adminToken: z.string().optional(),
+  })
+  .superRefine((data, ctx) => {
+    if (data.role === "admin") {
+      if (!data.adminToken) {
+        ctx.addIssue({
+          code: "custom",
+          message: "Token is mandatory",
+          path: ["adminToken"],
+        });
+      } else if (data.adminToken.length < 8 || data.adminToken.length > 10) {
+        ctx.addIssue({
+          code: "custom",
+          message: "Token must be between 8-10 characters",
+          path: ["adminToken"],
+        });
+      }
+    }
+  });
 
 const EditUser = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "ShaileshV",
-      email: "shailesh.example@gmail.com",
+      username: "",
+      email: "",
       phone: "",
-      location: "Bangalore, India",
-      role: "user",
+      location: "",
+      role: formSchema.shape.role.options[1],
     },
   });
 
@@ -63,16 +85,22 @@ const EditUser = () => {
     console.log(data);
   };
 
+  const userRole = useWatch({
+    control: form.control,
+    name: "role",
+  });
+
   return (
-    <SheetContent>
+    <SheetContent className="flex flex-col gap-2">
       <SheetHeader>
         <SheetTitle className="mb-4">Edit User</SheetTitle>
         <SheetDescription>
           Make changes to the user profile here...
         </SheetDescription>
       </SheetHeader>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="px-4">
-        <FieldGroup>
+
+      <form onSubmit={form.handleSubmit(onSubmit)} className="ml-4">
+        <FieldGroup className="gap-4">
           <Controller
             name="username"
             control={form.control}
@@ -121,7 +149,6 @@ const EditUser = () => {
                   {...field}
                   id={field.name}
                   aria-invalid={fieldState.invalid}
-                  placeholder="9876543210"
                 />
                 {fieldState.error && <FieldError errors={[fieldState.error]} />}
               </Field>
@@ -154,20 +181,44 @@ const EditUser = () => {
                   <UsersIcon size={16} />
                   Role
                 </FieldLabel>
-                <Select defaultValue="user">
+                <Select value={field.value} onValueChange={field.onChange}>
                   <SelectTrigger>
-                    <SelectValue />
+                    <SelectValue placeholder="Select a role" />
                   </SelectTrigger>
                   <SelectContent>
-                    <SelectItem value="user">User</SelectItem>
-                    <SelectItem value="admin">Admin</SelectItem>
+                    {/* 3. Map directly from the Zod Schema options */}
+                    {formSchema.shape.role.options.map((option) => (
+                      <SelectItem key={option} value={option}>
+                        {option.charAt(0).toUpperCase() + option.slice(1)}
+                      </SelectItem>
+                    ))}
                   </SelectContent>
                 </Select>
+
                 {fieldState.error && <FieldError errors={[fieldState.error]} />}
               </Field>
             )}
           />
-
+          {userRole === "admin" && (
+            <Controller
+              name="adminToken"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor={field.name}>Admin Token</FieldLabel>
+                  <Input
+                    {...field}
+                    id={field.name}
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Enter admin token"
+                  />
+                  {fieldState.error && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          )}
           <Button>Save</Button>
         </FieldGroup>
       </form>
